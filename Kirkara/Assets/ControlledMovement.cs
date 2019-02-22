412d18dd64fa7d19dd075643b529c6f9021f67d4
FreeMeow <paskoeyal@gmail.com>
2019-02-22 21:13:36 +0200

    Player-wagon implement
    implemented player and wagon
    work on wagon movement

---
+++ Kirkara/Assets/ControlledMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlledMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+
+        //transform.Translate(new Vector3(x, y, 0) * moveSpeed * Time.deltaTime);
+        //rb.AddForce(new Vector2(x, y) * moveSpeed * Time.deltaTime,ForceMode2D.Impulse);
+        rb.MovePosition((Vector2)transform.position + new Vector2(x, y) * Time.deltaTime * moveSpeed);
+        
+    }
+}
