390185b8ff8f57571d228fe8e2152538204fccaf
Superior-Potato <47921344+Superior-Potato@users.noreply.github.com>
2019-03-07 16:05:11 +0200

    start of enemy not finished

---
+++ Kirkara/Assets/Damagable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour
+{
+    public float maxLife;
+    private float currentLife;
+    public string type;
+    private string weakTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLife = maxLife;
+        switch (type)
+        {
+            case "fire":
+                weakTo = "Water";
+                break;
+        }//add more later.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentLife <= 0)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Damager penis = collision.gameObject.GetComponent<Damager>();
+        if(penis != null)
+        {
+            if (string.Compare(weakTo, penis.type) == 0)
+            {
+                currentLife -= penis.damage * 1.5f;
+            }
+            else
+            {
+                currentLife -= penis.damage;
+            }
+            Destroy(penis);
+        }
+    }
+}
---
+++ Kirkara/Assets/Damager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStatusEffect
+{
+    void Effecter();
+}
+
+public class Damager : MonoBehaviour
+{
+
+
+    public float damage;
+    public IStatusEffect status;
+    public string type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
