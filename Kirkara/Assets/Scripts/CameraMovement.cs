6a56878fd7907295d355814aa56b39292a55fba4
schonwoodiwiss <41946611+schonwoodiwiss@users.noreply.github.com>
2019-03-17 18:59:11 +0200

    implemented camera movement , bigger room, and upgraded tile placement
    included external tool that allows to place smart tiles based on rules that we can program, ask me for more detailes

---
+++ Kirkara/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float deadZone = .1f; 
+    public float cameraSpeed = .1f;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance((Vector2)transform.position, (Vector2)playerTransform.position) > deadZone) {
+            Vector2 newPos = Vector2.Lerp((Vector2)transform.position, (Vector2)playerTransform.position, cameraSpeed*Time.deltaTime);
+            transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+            Debug.Log("moving " + Vector2.Distance((Vector2)transform.position, newPos) + " units");
+            
+        }
+    }
+}
